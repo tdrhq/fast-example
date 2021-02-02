d66490bf9aba97740e9054179fc5a27b6da8dbc8
Arnold Noronha <arnold@tdrhq.com>
2021-02-02 01:09:44 -0800

    ..

--- gen.sh
+++ gen.sh
@@ -4,5 +4,5 @@ set -x
 
 mkdir screenshots || true
 # yrm screenshots/image.png
-convert -size 360x360 canvas:none -font "FreeMono" -pointsize 12 -fill xc:yellow -stroke xc:red -draw @ascii.txt screenshots/image.png
+convert -size 360x360 canvas:none -font "FreeMono" -pointsize 12 -draw @ascii.txt screenshots/image.png
 exiftool -all= screenshots/image.png
