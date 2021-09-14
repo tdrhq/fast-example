5c76c6479e7617ad17d38fdd22cdd7fd10eb903c
Arnold Noronha <arnold@tdrhq.com>
2021-09-14 11:03:43 -0700

    Don't need exiftool

--- gen.sh
+++ gen.sh
@@ -4,5 +4,4 @@ set -x
 
 mkdir screenshots || true
 # yrm screenshots/image.png
-convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt screenshots/image.png
-exiftool -all= screenshots/image.png
+convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
