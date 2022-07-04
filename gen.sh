52572c7a9f4b2ac25260e97c2a346c943ea7fe06
Arnold Noronha <arnold@tdrhq.com>
2022-07-04 15:18:57 -0400

    Use magick prefix

--- gen.sh
+++ gen.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 mkdir screenshots || true
-convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
+magick convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
