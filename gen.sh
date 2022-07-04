b47ff566f438b9f359a241dd9e1315a32109203d
Arnold Noronha <arnold@tdrhq.com>
2022-07-04 15:41:01 -0400

    make it work for magick

--- gen.sh
+++ gen.sh
@@ -3,4 +3,10 @@ set -e
 set -x
 
 mkdir screenshots || true
-magick convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
+MAGICK=""
+
+if magick --help ; then
+    MAGICK=magick
+fi
+
+$MAGICK convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
