6643bc9e264c2366fac09517c6655dc9e8b80d92
Arnold Noronha <arnold@tdrhq.com>
2023-02-24 19:14:56 -0500

    ..

--- gen.sh
+++ gen.sh
@@ -8,12 +8,6 @@ mkdir screenshots || true
 MAGICK=""
 env
 
-if [ x"$CIRCLE_PULL_REQUEST" = x ] && [ x"$CI_MERGE_REQUEST_IID" = x ] ; then
-    # just testing failure modes
-    true
-    #exit 1
-fi
-
 if magick --help ; then
     MAGICK=magick
 fi
