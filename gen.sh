09658acec2b688b62e0e7f8912527d91f2bd3773
Arnold Noronha <arnold@tdrhq.com>
2023-02-24 19:23:55 -0500

    ..

--- gen.sh
+++ gen.sh
@@ -14,4 +14,6 @@ fi
 
 echo Before convert call
 
+$MAGICK convert --version
+
 $MAGICK convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
