3be209da6b797a6821d8c43b13c8b2ce64cf4a5b
Arnold Noronha <arnold@screenshotbot.io>
2025-07-28 22:21:40 -0400

    ...

--- gen.sh
+++ gen.sh
@@ -21,6 +21,24 @@ echo Before convert call
 
 $MAGICK convert --version
 
-$MAGICK convert -size 360x360 xc:white -font "FreeMono" -pointsize 12 -fill black -stroke black -draw @ascii.txt -strip screenshots/image.png
+
+FONT=Helvetica
+
+$MAGICK convert -size 375x812 xc:"#F2F2F7" \
+        -antialias \
+  -font "$FONT" -pointsize 24 -fill "#007AFF" -gravity North -annotate +0+120 "Welcome Back" \
+  -font "$FONT" -pointsize 16 -fill "#1C1C1E" -gravity North -annotate +0+180 "Sign in to your account" \
+  -fill "#FFFFFF" -stroke "#E5E5EA" -strokewidth 1 -draw "roundrectangle 40,260 335,310 8,8" \
+  -font "$FONT" -pointsize 16 -fill "#8E8E93" -gravity NorthWest -annotate +55+280 "Email" \
+  -fill "#FFFFFF" -stroke "#E5E5EA" -strokewidth 1 -draw "roundrectangle 40,330 335,380 8,8" \
+  -font "$FONT" -pointsize 16 -fill "#8E8E93" -gravity NorthWest -annotate +55+350 "Password" \
+  -font "$FONT" -pointsize 14 -fill "#007AFF" -gravity NorthWest -annotate +55+400 "Forgot Password?" \
+  -fill "#007AFF" -stroke none -draw "roundrectangle 40,440 335,490 8,8" \
+  -font "$FONT" -pointsize 18 -fill "#FFFFFF" -gravity NorthWest -annotate +147+455 "Sign In" \
+  -fill "#111111" -draw @ascii.txt \
+  -strip screenshots/image.png \
+
+
+
 
 echo After convert call
